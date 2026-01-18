fca85f2eb50ea3a8c3a95e0021bf5a8d895f40cd
prismaaryadwi <prisma.aryadwi@gmail.com>
2026-01-18 22:15:24 +0700

    bismillahfix2

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/uploads/**",
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
